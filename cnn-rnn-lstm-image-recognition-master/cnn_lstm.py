7a6c49bf77b632cd8156e5937565246afd2a17c0
Samuel Oluwadare Folorunsho <samuel@wirelessprv-10-193-245-188.near.illinois.edu>
2022-05-12 20:37:01 -0500

    Adding new commit

--- cnn-rnn-lstm-image-recognition-master/cnn_lstm.py
+++ cnn-rnn-lstm-image-recognition-master/cnn_lstm.py
@@ -1,3 +1,4 @@
+from torch import dropout
 import torch.nn as nn
 import torch.nn.functional as f
 from cnn import CNN
@@ -25,16 +26,23 @@ class CnnLstm(nn.Module):
         r_out2 = self.linear(r_out[:, -1, :])
         return f.log_softmax(r_out2, dim=1)
 
+import torch
 
 num_classes = 4
 class AlexLstm(nn.Module):
     def __init__(self):
         super(AlexLstm, self).__init__()
         self.cnn = CNN()
+        #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        #model = torch.hub.load('pytorch/vision:v0.10.0', 'alexnet', pretrained=False)
+        #model.classifier._modules['6'] = nn.Linear(4096, 4)
+        #model.eval()
+        #self.cnn = model()
         self.rnn = nn.LSTM(
             input_size=4096,
             hidden_size=256,
-            num_layers=1,
+            num_layers=4,
+            dropout =0.5,
             batch_first=True)
         self.linear = nn.Linear(64, num_classes)
 
